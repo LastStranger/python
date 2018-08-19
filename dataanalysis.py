f4ec66b4208546a47d79f997238cb3a7c127d8e3
LZ <1014456062@qq.com>
2018-08-19 23:46:18 +0800

    a GUI and and a bg_music but failed warp the file

---
+++ dataanalysis.py
@@ -0,0 +1,14 @@
+# import signal
+# import pyopenfdm
+# import pandas
+# import glob2
+#
+#
+#
+# file_list = glob2.glob("D:\\Analysis\\*.txt")
+# for file in file_list:
+#     df = pandas.read_csv(file)
+#     m = df.mean()
+#     m = float(m)
+#     print(m)
+
--- demo.py
+++ demo.py
@@ -1,9 +1,31 @@
-fileList = list(range(200, 210, 2))
-print(fileList)
-another_fileList = fileList[1:2]
-print(fileList)
-print(another_fileList)
-print(fileList[:3])
-print(fileList[3:])
-str = "what we have lost will never"
-print(str[5])
+import tkinter
+from pygame import mixer
+
+from tkinter.filedialog import askopenfilename
+
+
+def browse():
+    global in_file
+    in_file = askopenfilename()
+
+
+def kml_function():
+    with open(in_file, 'w') as file:
+        file.write("万鹏萌萌哒")
+
+
+root = tkinter.Tk()
+root.title("FUCK")
+label = tkinter.Label(root, text="打开一个txt文件")
+label.pack()
+browse_button = tkinter.Button(root, text="Browse", command=browse)
+browse_button.pack()
+kml_button = tkinter.Button(root, text="world", command=kml_function)
+kml_button.pack()
+
+mixer.init()
+mixer.music.load("Funky Stars.mp3")
+mixer.music.play()
+
+root.mainloop()
+
